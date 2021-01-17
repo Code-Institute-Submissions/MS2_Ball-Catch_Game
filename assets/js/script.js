16beb5f9ca1fb36de4e8637f3c77edd7c38381f9
TomDillane <tommy.c.dillane@gmail.com>
2021-01-17 18:24:17 +0000

    Feat: Ninja moves back and forth without leaving a trail

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,9 @@ document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
 let canvas = document.getElementById("canvas");
+let canvas2 = document.getElementById("warrior");
 let ctx = canvas.getContext("2d");
+let ctx2 = canvas2.getContext("2d");
 //let x = Math.floor(Math.random() * (canvas.width - 5));
 //let y = 5;
 let r = 5;
@@ -30,6 +32,8 @@ let fireBallRate = 2000;
 let lastFireBall = -1;
 let colors = ["red", "green", "orange"];
 let warriorx = 100;
+let rightPressed = false;
+let leftPressed = false;
 
 
 
@@ -65,11 +69,12 @@ function fireballs() {
 }
 
 function warrior() {
-    let canvas2 = document.getElementById("warrior");
-    let ctx = canvas2.getContext("2d");
+    //let canvas2 = document.getElementById("warrior");
+   // let ctx = canvas2.getContext("2d");
     img = new Image();
     img.onload = function() {
-        ctx.drawImage(img, warriorx, -20, 80, 200);
+        ctx2.clearRect(0, 0,  canvas2.width, canvas2.height);
+        ctx2.drawImage(img, warriorx, -20, 80, 200);
     }
     img.src = "assets/images/defender.png"
 }
@@ -99,17 +104,18 @@ function draw() {
         lastFireBall = timeStamp;
         createFireBalls();
     }
-    requestAnimationFrame(draw);
+    //requestAnimationFrame(draw);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
     fireballs();
     warrior();
     if (rightPressed) {
         warriorx += 7;
     }
-
-            else if (leftPressed) {
-    warriorx -= 7;
-            }
+    else if (leftPressed) {
+        warriorx -= 7;
+    }
+    requestAnimationFrame(draw);
 }
 draw();
 
